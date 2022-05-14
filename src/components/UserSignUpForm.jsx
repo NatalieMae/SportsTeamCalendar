e6d2be0d1575a94bb8cb785558842e8f4a298f6e
RDM_ <rafadiazm77@gmail.com>
2022-05-14 12:45:20 -0500

    data captured from signupform

--- src/components/UserSignUpForm.jsx
+++ src/components/UserSignUpForm.jsx
@@ -8,6 +8,19 @@ function UserSignUp(){
     const [title, setTitle] = useState('Sign Up!')
     const [subtitle, setSubTitle] = useState('Please fill out the form')
 
+    const [signUpCredentials, setSignUpCredentials] = useState({})
+
+    const handleData=({target})=>{
+        const{name,value} = target
+        console.log(name,value)
+        setSignUpCredentials({[name]: value})
+
+    }
+
+
+
+
+
     const handleSentFomMessage = ()=>{
         setSubTitle('Your info has been sent!')
     }
@@ -21,18 +34,18 @@ function UserSignUp(){
                     <form action= "">
                         <div >
                             <label htmlFor="firstName">First Name :</label>
-                            <input id="firstName" name="firstName"  />
+                            <input id="firstName" name="firstName" onChange={handleData} />
                         </div>
                         <div>
                             <label htmlFor="lastName">Last Name :</label>
-                            <input type="text" id = 'lastName' name ="lastName"  />
+                            <input type="text" id = 'lastName' name ="lastName" onChange ={handleData} />
                         </div>
                         <div>
-                            <label htmlFor="city">DOB:</label>
-                            <input id="city" name="city" />
+                            <label htmlFor="DOB">Date of Birth:</label>
+                            <input id="DOB" type= "date" name="dob" onChange ={handleData} />
                         </div>
                         <label htmlFor="team"> Select Your Team</label>
-                        <select name ='team' id='team'>
+                        <select name ='team' id='team' onChange ={handleData}>
                                 <option value="Select">Select team</option>
                                 <option value="team1">team 1</option>
                                 <option value="team2">team 2</option>
@@ -41,11 +54,15 @@ function UserSignUp(){
                         
                         <div>
                             <label htmlFor="userEmailAddress">Email :</label>
-                            <input type="userEmailAddress" id = 'userEmailAddress' name = 'userEmailAddress'/>
+                            <input type="email" id = 'userEmailAddress' name = 'userEmailAddress' onChange ={handleData}/>
+                        </div>
+                        <div>
+                            <label htmlFor="userName">Username</label>
+                            <input type="password" name = "userName" onChange ={handleData} maxLength ={20} />
                         </div>
                         <div>
                             <label htmlFor="userPassword">Password</label>
-                            <input type="password" />
+                            <input type="password" name ="userPassword" onChange ={handleData} maxLength ={20} />
                         </div>
                     
                         <div>
