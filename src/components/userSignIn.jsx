ef2c7214ccaac0bbc24643ca528af6490c59aa6c
RDM_ <rafadiazm77@gmail.com>
2022-05-14 10:39:14 -0500

    get values from from

--- src/components/userSignIn.jsx
+++ src/components/userSignIn.jsx
@@ -5,31 +5,38 @@ import {useState, useEffect} from 'react';
 
 function UserSignIn(){
 
-    const [username, setUsername] = useState()
-    const [userPassword, setUserPassword] = useState()
+    const [userCredentials, setUserCredentials] = useState({})
+  
     const handleSubmit = (e)=>{
         e.preventDefault();
         console.log('submitted')
     }
-    const handleData =()=> {
+    const handleData =({target})=> {
+        
+        const {name, value} = target
+        console.log(name,value)
+        setUserCredentials({[name]: value});
+        console.log(userCredentials)
 
     }
 
     
-
-
-    
     return( 
+        
     
     <div>
+        
+
        <form  action="" onSubmit = {handleSubmit}>
            <label htmlFor = "username">
                <p>Username</p>
-               <input type="text" onChange = {e => setUsername(e.target.value)}  />
+               <input name = 'userName' type="text" onChange = {handleData}  />
            </label>
+
+
            <label htmlFor="userPassword">
                <p>Password</p>
-               <input type="password" onChange = {e => setUserPassword(e.target.value)} />
+               <input name = 'userPassword' type="password" onChange = {handleData} />
            </label>
            <br />
            <input type="submit" value="Entry" />
