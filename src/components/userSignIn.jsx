cbadacaf5ca958121680791a0efac0f51b7ec1bd
RDM_ <rafadiazm77@gmail.com>
2022-05-14 11:39:08 -0500

    maxLenght, value captured for signin

--- src/components/userSignIn.jsx
+++ src/components/userSignIn.jsx
@@ -4,22 +4,27 @@ import {useState, useEffect} from 'react';
 
 
 function UserSignIn(){
-
+//get values from username & password
     const [userCredentials, setUserCredentials] = useState({})
-  
+    const handleData =({target})=> {     
+            
+            const {name, value} = target
+            console.log(name,value)
+            setUserCredentials({[name]: value});
+            console.log(userCredentials)
+
+        }
+ 
+    // Prevent submission
     const handleSubmit = (e)=>{
         e.preventDefault();
-        console.log('submitted')
-    }
-    const handleData =({target})=> {
-        
-        const {name, value} = target
-        console.log(name,value)
-        setUserCredentials({[name]: value});
-        console.log(userCredentials)
-
+        console.log('not sent')
+       
     }
 
+
+
+   
     
     return( 
         
@@ -30,14 +35,13 @@ function UserSignIn(){
        <form  action="" onSubmit = {handleSubmit}>
            <label htmlFor = "username">
                <p>Username</p>
-               <input name = 'userName' type="text" onChange = {handleData}  />
-           </label>
-
-
-           <label htmlFor="userPassword">
+                <input name = 'userName' type="text" onChange = {handleData} maxLength= {10} required />
                <p>Password</p>
-               <input name = 'userPassword' type="password" onChange = {handleData} />
+                <input name = 'userPassword' type="password" onChange = {handleData} maxLength={20} required />
            </label>
+
+
+           
            <br />
            <input type="submit" value="Entry" />
 
