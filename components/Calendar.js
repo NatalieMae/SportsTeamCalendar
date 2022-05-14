7e2c78e778559ebeae0f83ab92a81e7a9c637959
Natalie Leonard <natalie@Natalies-MBP.attlocal.net>
2022-05-14 09:48:03 -0500

    calendar imported

---
+++ components/Calendar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Calendar from "react-calendar";
+
+
+function Calendar() {
+    const date = (Calendar.map({date
+
+    }))
+
+    return (
+        <div className="pretty-calendar">
+
+        </div>
+    )
+}
+
+export default Calendar;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Calendar from 'react-calendar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        "Snack APP!"
       </header>
+      <main>
+        <div className='pretty-calendar'>
+        <Calendar />
+        </div>
+      </main>
     </div>
   );
 }
