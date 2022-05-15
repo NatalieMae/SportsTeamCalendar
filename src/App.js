0eca94fd16a1d20ca39c00cfb5919706548160c3
Natalie Leonard <natalie@Natalies-MBP.attlocal.net>
2022-05-15 13:56:17 -0500

    detailed info added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
+import React from 'react';
 import './App.css';
 import WelcomePage from './components/WelcomePage';
 import UserSignUp from './components/UserSignUpForm';
 import UserSignIn from './components/userSignIn';
-import React from 'react';
 // import {Calendar as ReactCalendar} from 'react-calendar';
 import Calendarview from './components/Calendarview';
-// import 'react-calendar/dis/Calendar.css';
+
 import Snack from './components/Snack'
 import Beverage from './components/Beverage'
 
 
 function App() {
 
- const welcomeTitle = 'Welcome to the Food App'
+  const welcomeTitle = 'Welcome to the Food App'
 
   return (
-    <div>
-    <WelcomePage title = {welcomeTitle} />
 
-    <UserSignIn />
-    <UserSignUp />
-      <div className=''>
-      <main>
-        <div className='pretty-calendar'>
-          <Calendarview />
-          <Snack />
-          <Beverage />
-        </div>
-      </main>
+    <div>
+    <main>
+      <div className='pretty-calendar'>
+        <WelcomePage title = {welcomeTitle} />
+        <UserSignIn />
+        <UserSignUp />
+        <Calendarview />
+        <Snack />
+        <Beverage />
+      </div>
+    </main>
     </div>
   );
 }
--- src/components/Calendarview.js
+++ src/components/Calendarview.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 import {Calendar as ReactCalendar} from 'react-calendar';
-
+import 'react-calendar/dis/Calendar.css';
+import './App.css';
 function Calendarview() {
-    const [value, onChange] = useState(new Date());
+    const [date, setDate] = useState([
+        new Date(),
+    ]);
 
     return (
         <div className="pretty-calendar">
-            {/* <input type= "checkbox" onChange={()}></input> */}
-            <ReactCalendar onChange={onChange} value={value} />
+            <h1 className="text-center">Team Game Day Information</h1>
+        <div className="calendar-container">
+            <ReactCalendar 
+                onChange={setDate} 
+                value={date}
+                maxDate={new Date()}
+                minDetail='year'
+            />
+        </div>
         </div>
     );
 }
