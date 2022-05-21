8da36f99f734ea28e2bd4684bea19b61cb60b86d
Chris O'Brien <you@example.comchristopherwobrien03@gmail.com>
2022-05-21 11:08:25 -0500

    added link to sign up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -227,7 +227,7 @@ export default function PrimaryNavbar() {
               </MenuItem>
               <MenuItem onClick={(e) => {
                 e.preventDefault();
-                window.location.href = "http://localhost:3000/";
+                window.location.href = "http://localhost:3000/auth/sign-up";
               }}>
                 <Typography textAlign="center">Sign Up</Typography>
               </MenuItem>
