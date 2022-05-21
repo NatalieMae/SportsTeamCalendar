0d92fb913394f92b0a84018155aebd55a9eeaaaf
RDM_ <rafadiazm77@gmail.com>
2022-05-20 19:09:18 -0500

    allergic status radio buttons

--- src/components/AddPlayerForm.jsx
+++ src/components/AddPlayerForm.jsx
@@ -6,14 +6,21 @@ import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 
 
+    
 const teams =['None','team1','team2', 'team3', 'team4']
 
 function AddPlayerForm(){
 
     const [currentSelectedTeam, setCurrentSelectedTeam] = useState('None')
     const [data, setData] = useState('none')
+    const [allergyStatus, setAllergyStatus] =useState(false)
 
     const handleTeamChange = (event)=>{
         setCurrentSelectedTeam(event.target.value)
@@ -23,6 +30,11 @@ function AddPlayerForm(){
     const handleDataChange = (event) =>{
        setData(event.target.value)
        console.log(event.target.value)
+    }
+    const handleAllergicStatus =(e)=>{
+        setAllergyStatus(e.target.value)
+        console.log(e.target.value)
+
     }
 
     // const handleData=({target})=>{
@@ -70,6 +82,19 @@ function AddPlayerForm(){
                         onChange={handleDataChange}
                         maxLength = {10}
                         />
+                       
+                       <FormControl>
+                            <FormLabel id="demo-controlled-radio-buttons-group">Allergy Status</FormLabel>
+                            <RadioGroup
+                                aria-labelledby="demo-controlled-radio-buttons-group"
+                                name="controlled-radio-buttons-group"
+                                value={allergyStatus}
+                                onChange={handleAllergicStatus}
+                            >
+                                <FormControlLabel value="false" control={<Radio />} label="No Allergies" />
+                                <FormControlLabel value="true" control={<Radio />} label="Allergies" />
+                            </RadioGroup>
+                        </FormControl>
 
                         <InputLabel id="Select a team">Select Player's Team</InputLabel>
                             <Select
